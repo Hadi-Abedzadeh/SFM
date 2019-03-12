eac02f32c497af217caee9d180bd8a18b89b010d
danialz <danialz804@gmail.com>
2019-03-12 09:49:15 +0330

    menu fixes

--- resources/views/sfm/frontend-fa/frontend-index.blade.php
+++ resources/views/sfm/frontend-fa/frontend-index.blade.php
@@ -75,13 +75,13 @@
         </nav>
 		<div id="fullmenu">
 		<div class="fullmenulink">
-			<a href="http://127.0.0.1:8000/brand">صفحه اصلی</a>
-			<a href="http://127.0.0.1:8000/about-us">درباره ما</a>
-			<a href="http://127.0.0.1:8000/catalog">کاتالوگ</a>
-			<a href="http://127.0.0.1:8000/products">محصولات</a>
-			<a href="http://127.0.0.1:8000/blog">بلاگ</a>
-			<a href="http://127.0.0.1:8000/news">اخبار</a>
-			<a href="http://127.0.0.1:8000/contact-us">تماس با ما</a>
+			<a href="{{ route('frontend') }}">صفحه اصلی</a>
+			<a href="{{ route('frontend.about.index') }}">درباره ما</a>
+			<a href="{{ route('catalog') }}">کاتالوگ</a>
+			<a href="{{ route('frontend.product.index') }}">محصولات</a>
+			<a href="{{ route('frontend.blog.index') }}">بلاگ</a>
+			<a href="{{ route('frontend.news.index') }}">اخبار</a>
+			<a href="{{ route('frontend.contact-us.index') }}">تماس با ما</a>
 		</div>
 		<div class="fullmenucircle1"></div>
 		<div class="fullmenucircle2"></div>
